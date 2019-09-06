f422541fbf5c82f69c9e3b537716274a3a12ccfa
Kyle E. Mitchell <kyle@kemitchell.com>
2019-09-05 20:15:53 -0700

    Fix mention of `npm fund` on `npm install`

--- test/tap/install-mention-funding.js
+++ test/tap/install-mention-funding.js
@@ -7,11 +7,30 @@ var common = require('../common-tap.js')
 
 var testdir = common.pkg
 var fixture = new Tacks(Dir({
+  node_modules: Dir({
+    a: Dir({
+      'package.json': File({
+        name: 'a',
+        version: '1.0.0',
+        dependencies: {
+          b: '1.0.0'
+        }
+      }),
+      node_modules: Dir({
+        b: Dir({
+          'package.json': File({
+            name: 'b',
+            version: '1.0.0'
+          })
+        })
+      })
+    })
+  }),
   'b-src': Dir({
     'package.json': File({
       name: 'b',
-      funding: 'https://example.com/funding.json',
-      version: '1.0.0'
+      version: '1.0.0',
+      funding: 'http://example.com/fund.json'
     })
   })
 }))
@@ -30,12 +49,12 @@ test('setup', function (t) {
   t.end()
 })
 
-test('install', function (t) {
+test('install-report', function (t) {
   common.npm(['install', '--no-save', './b-src'], {cwd: testdir}, function (err, code, stdout, stderr) {
     if (err) throw err
     t.is(code, 0, 'installed successfully')
     t.is(stderr, '', 'no warnings')
-    t.includes(stdout, '`npm fund`', 'mentions `npm fund`')
+    t.includes(stdout, '`npm fund`', 'metions `npm fund`')
     t.end()
   })
 })
